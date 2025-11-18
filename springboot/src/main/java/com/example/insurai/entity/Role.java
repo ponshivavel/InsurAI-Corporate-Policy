10933420454917a6a3a8fdeee149b863c44048a2
PONSHIVAVEL S K <ponshivavelsk@gmail.com>
2025-11-18 18:06:59 +0530

    Delete springboot/src/main directory

--- springboot/src/main/java/com/example/insurai/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.insurai.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- springboot/src/main/java/com/example/insurai/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.insurai.repository;
-
-import com.example.insurai.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
